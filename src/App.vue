ef0ab53c0a781bbb565c46f76b5f62cb3d9d7ca0
marcodedemo <marco.dedemo@gmail.com>
2023-05-29 14:39:24 +0200

    feat: milestone 4 & 5

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+/* --------------------------------- js --------------------------------- */
+<script>
+import axios from 'axios';
+
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  data() {
+    return {
+
+      projects: [],
+
+    }
+  },
+
+  components : {
+    ProjectCard,
+  },
+
+
+
+  created(){
+
+    this.apiCall();
+  },
+
+  methods: {
+
+    apiCall(){
+
+      axios.get('http://127.0.0.1:8000/api/projects').then(res =>{
+
+        this.projects = res.data.results;
+
+        console.log(this.projects)
+
+      })
+    },
+  },
+
+}
+
+
+
 </script>
 
+
+/* ---------------------------------- html ---------------------------------- */
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+<div class="container">
+
+  <h1 class="py-3">Projects</h1>
+  
+  <div id="card-container" class="d-flex flex-wrap gap-4">
+    
+    <ProjectCard :project="project" v-for="project in projects"></ProjectCard>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+</div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+/* ----------------------------------- css ---------------------------------- */
+<style lang="scss">
+
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,51 @@
+
+/* --------------------------------- js --------------------------------- */
+<script>
+import axios from 'axios';
+
+
+export default {
+  data() {
+    return {
+
+      imgBaseUrl: 'http://127.0.0.1:8000/',
+
+      
+
+    }
+  },
+
+  props:{
+    project: Object,
+  },
+
+
+}
+
+
+
+</script>
+
+
+/* ---------------------------------- html ---------------------------------- */
+<template>
+
+<div class="card" style="width: 18rem;">
+      
+    <img :src="project.cover_image ? this.imgBaseUrl + 'storage/' + project.cover_image : 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg'" class="card-img-top" alt="">
+    
+    <div class="card-body">
+        <h5 class="card-title">{{project.title}}</h5>
+        <p class="card-text">{{ project.description }}</p>
+        <a :href="project.link" class="btn btn-primary">Github Link</a>
+    </div>
+
+</div>
+
+</template>
+
+
+/* ----------------------------------- css ---------------------------------- */
+<style lang="scss">
+
+</style>
