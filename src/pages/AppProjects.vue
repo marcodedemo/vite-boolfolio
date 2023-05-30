9948b25c09fd43c6f99f25f1ccb1b12ee43b801e
marcodedemo <marco.dedemo@gmail.com>
2023-05-30 14:56:16 +0200

    feat: added spinner on loading

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -67,24 +67,34 @@ export default {
 /* ---------------------------------- html ---------------------------------- */
 <template>
 
-<div class="container">
+  <div class="container">
 
-<h1 class="py-3">Projects</h1>
+    <h1 class="py-3">Projects</h1>
 
-<div id="card-container" class="d-flex flex-wrap gap-4">
-  
-  <ProjectCard :project="project" v-for="project in projects"></ProjectCard>
-  
-</div>
+    <div v-if="this.projects.length > 0" id="cards-page-container">
 
-<div id="pagination" class="d-flex justify-content-center gap-5 py-3 fs-2 ">
+      <div id="card-container" class="d-flex flex-wrap gap-4">
+        
+        <ProjectCard :project="project" v-for="project in projects"></ProjectCard>
+        
+      </div>
+      
+      <div id="pagination" class="d-flex justify-content-center gap-5 py-3 fs-2 ">
+        
+        <div @click="prevPage" class="back"><i class="fa-solid fa-arrow-left"></i></div>
+        <div @click="nextPage" class="next"><i class="fa-solid fa-arrow-right"></i></div>
+        
+      </div>
+      
+    </div>
 
-  <div @click="prevPage" class="back"><i class="fa-solid fa-arrow-left"></i></div>
-  <div @click="nextPage" class="next"><i class="fa-solid fa-arrow-right"></i></div>
+    <div v-else class="loading">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
 
-</div>
-
-</div>
+  </div>
 
 
 </template>
@@ -92,5 +102,10 @@ export default {
 
 /* ----------------------------------- css ---------------------------------- */
 <style lang="scss" scoped>
+  .loading{
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+  }
 </style>
