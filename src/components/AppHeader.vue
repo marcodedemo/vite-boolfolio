fd6f5df280fc9e78e09402077c3cc5ee2c45787b
marcodedemo <marco.dedemo@gmail.com>
2023-06-08 15:37:49 +0200

    feat: added sass icon and modified header texts

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,15 +19,15 @@ export default {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
-    <router-link class="navbar-brand" :to="{name: 'home'}">Boolfolio</router-link>
+    <router-link class="navbar-brand" :to="{name: 'home'}">Marco De Demo</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
-        <router-link class="nav-link" :to="{name: 'projects.index'}">Projects</router-link>
-        <router-link class="nav-link" :to="{name: 'about'}">About Me</router-link>
+        <router-link class="nav-link" :to="{name: 'projects.index'}">Progetti</router-link>
+        <router-link class="nav-link" :to="{name: 'about'}">Chi sono</router-link>
 
 
       </div>
